0e670556f2f3dd4bd977ca6c8105fa3412803519
Gabriel Honda <gabrielhondacba@gmail.com>
2021-04-04 16:34:58 -0400

    aula: exemplo simples de lambda 💥

---
+++ lambda-and-functional-interface/src/main/java/org/da0hn/LambdaExample.java
@@ -0,0 +1,27 @@
+package org.da0hn;
+
+/**
+ * Created by daohn on 04/04/2021
+ * @author daohn
+ * @since 04/04/2021
+ * @version 1.0.0
+ */
+public class LambdaExample {
+
+  public static void main(String[] args) {
+    var myRunnable = new MyRunnable();  // Não é mais necessário após Java 8
+
+    /*
+     * Funções possuem 4 propriedades
+     *
+     *  - Nome da função
+     *  - Lista de parâmetros
+     *  - Corpo da função
+     *  - Retorno de um tipo
+     */
+    Thread thread = new Thread(() -> System.out.println("Thread executada"));
+
+    thread.start();
+  }
+
+}

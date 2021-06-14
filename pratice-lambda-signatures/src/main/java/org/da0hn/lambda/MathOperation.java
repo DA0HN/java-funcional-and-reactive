1551ae0c9933cf63605f21acfd171553181bb285
Gabriel Honda <gabrielhondacba@gmail.com>
2021-06-14 16:51:44 -0400

    aula: lambda com parâmetro e sem retorno

---
+++ pratice-lambda-signatures/src/main/java/org/da0hn/lambda/MathOperation.java
@@ -0,0 +1,14 @@
+package org.da0hn.lambda;
+
+/**
+ * Created by daohn on 14/06/2021
+ * @author daohn
+ * @since 14/06/2021
+ */
+public interface MathOperation {
+
+
+  void operation(int a, int b);
+
+
+}

953c650acffde2392a7ae7f2ae7980b32b0d2848
Gabriel Honda <gabrielhondacba@gmail.com>
2021-07-10 10:13:08 -0400

    feat: adiciona implementação da interface Comparable FnRx-14

--- collectors-for-data-processing-in-depth/src/main/java/org/da0hn/domain/Employee.java
+++ collectors-for-data-processing-in-depth/src/main/java/org/da0hn/domain/Employee.java
@@ -15,5 +15,17 @@ public record Employee(
   String designation,
   LocalDate joiningDate,
   Double salary
-) {
+) implements Comparable<Employee> {
+  @Override public int compareTo(Employee o) {
+//    if(this.id < o.id()) {
+//      return -1;
+//    }
+//    else if(this.id > o.id()) {
+//      return 1;
+//    }
+//    else {
+//      return 0;
+//    }
+    return this.id.compareTo(o.id());
+  }
 }

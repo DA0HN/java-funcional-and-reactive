8676b657e470402bb47c51ba99c960d2c151846b
Gabriel Honda <gabrielhondacba@gmail.com>
2021-07-10 09:38:21 -0400

    aula: terminal analogous collectors-1 FnRx-14

---
+++ collectors-for-data-processing-in-depth/src/main/java/org/da0hn/usecases/CollectorsInActionUseCase.java
@@ -0,0 +1,29 @@
+package org.da0hn.usecases;
+
+import org.da0hn.domain.Employee;
+import org.da0hn.utils.EmployeeDataFactory;
+
+import java.util.stream.Collectors;
+
+/**
+ * @author daohn
+ * @since 10/07/2021
+ */
+public class CollectorsInActionUseCase {
+
+  public static void main(String[] args) {
+    var employees = EmployeeDataFactory.create();
+
+    var names = employees.stream()
+      .map(Employee::name)
+      .collect(Collectors.toList());
+
+    names.forEach(System.out::println);
+
+    var designations = employees.stream().map(Employee::designation).collect(Collectors.toSet());
+
+    designations.forEach(System.out::println);
+
+  }
+
+}

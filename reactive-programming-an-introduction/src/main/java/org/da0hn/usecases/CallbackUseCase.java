2e673c4400586260646a92be7ae687eeb56b2867
Gabriel Honda <gabrielhondacba@gmail.com>
2021-07-13 09:27:28 -0400

    aula: callback hell FnRx-18

---
+++ reactive-programming-an-introduction/src/main/java/org/da0hn/usecases/CallbackUseCase.java
@@ -0,0 +1,43 @@
+package org.da0hn.usecases;
+
+import java.util.concurrent.TimeUnit;
+
+/**
+ * @author daohn
+ * @since 13/07/2021
+ */
+public class CallbackUseCase {
+
+  public static void main(String[] args) {
+
+    System.out.println("Main thread is running.");
+
+    Runnable r = () -> {
+      new CallbackUseCase().runningAsync(() -> {
+        System.out.println("Callback called!");
+      });
+    };
+
+    new Thread(r).start();
+
+    sleep(2000);
+
+    System.out.println("Main thread completed.");
+  }
+
+  public void runningAsync(ICallback callback) {
+    System.out.println("I am running in separate thread.");
+    sleep(1000);
+    callback.call();
+  }
+
+  private static void sleep(int duration) {
+    try {
+      TimeUnit.MILLISECONDS.sleep(duration);
+    }
+    catch(InterruptedException e) {
+      e.printStackTrace();
+    }
+  }
+
+}
---
+++ reactive-programming-an-introduction/src/main/java/org/da0hn/usecases/ICallback.java
@@ -0,0 +1,11 @@
+package org.da0hn.usecases;
+
+/**
+ * @author daohn
+ * @since 13/07/2021
+ */
+public interface ICallback {
+
+  void call();
+
+}
